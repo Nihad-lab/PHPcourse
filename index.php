0671a8eab2508c8c2e3a9d4a8ab02972f3986577
Nihad-lab <mamedovnihad306@gmail.com>
2025-12-24 22:40:52 +0300

    19

--- index.php
+++ index.php
@@ -1,19 +1,12 @@
-<form action="index.php" method="post">
-    <label>Enter a country:</label><br>
-    <input type="text" name="country"><br>
-    <input type="submit" value="Submit">
-</form>
-
 <?php
 $capitals = array(
     "USA" => "Washington D.C.",
     "Japan" => "Kyoto",
-    "South Korea" => "Seoul",
     "India" => "New Delhi"
 );
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $country = $_POST['country'];
-    $capital = $capitals[$country] ?? "Capital not found.";
+    $capital = $capitals[$country];
     echo "The capital of {$country} is {$capital}.";
 }
 ?>
