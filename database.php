be87fd7a7c08477bc024fa59366a25de98152f5a
Nihad-lab <mamedovnihad306@gmail.com>
2025-12-24 23:25:07 +0300

    34

--- database.php
+++ database.php
@@ -46,4 +46,18 @@ try {
 }
 mysqli_close($conn);
 ?>
+<?php
+include 'database.php';
+$sql = "SELECT * FROM users WHERE user='SpongeBob'";
+$result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
+    echo "ID: {$row['id']}<br>";
+    echo "Username: {$row['user']}<br>";
+} else {
+    echo "No user found.";
+}
+mysqli_close($conn);
+?>
+
 
