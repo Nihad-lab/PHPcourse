26430399a8895df936d01ff11bba2ea0a59bfb53
Nihad-lab <mamedovnihad306@gmail.com>
2025-12-24 22:34:13 +0300

    14

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
+<form action="index.php" method="post">
+    <label>Enter a number to count up to:</label><br>
+    <input type="text" name="counter"><br>
+    <input type="submit" value="Start">
+</form>
 <?php
-for ($i = 0; $i < 5; $i++) {
-    echo "Hello {$i}<br>";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $counter = $_POST['counter'];
+    for ($i = 1; $i <= $counter; $i++) {
+        echo $i . "<br>";
+    }
 }
 ?>
 
