c741315461f740397b6eb4cf5ab08d7b19c49d3f
Nihad-lab <mamedovnihad306@gmail.com>
2025-12-24 22:53:33 +0300

    24

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
-function happyBirthday($first_name, $age) {
-    echo "Happy birthday dear {$first_name}<br>";
-    echo "You are {$age} years old!<br>";
-}
-happyBirthday("SpongeBob", 30);
+$username = " Bro Code ";
+$username = strtolower(trim($username));
+echo $username; // "bro code"
+$phone = "123-456-7890";
+$phone = str_replace("-", "", $phone);
+echo $phone; // "1234567890"
 ?>
